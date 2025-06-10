9d800fadb635f1ff9cf20bbfb26bd3ee13063eb3
Vladislav M <mymail@mail.ru>
2025-06-11 02:32:54 +0300

    new branch

--- yt_downloader/back/yt_downloader/yt_app/tests.py
+++ yt_downloader/back/yt_downloader/yt_app/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import unittest
+from yt_downloader.back.yt_downloader.yt_app.services.yt_downloader import list_formats
+from yt_dlp import DownloadError
 
-# Create your tests here.
+class TestYt(unittest.TestCase):
+    def test_list_formats(self):
+        with self.assertRaises(DownloadError):
+            list_formats("https://example.com/video/123")
+        self.assertEqual(type(list_formats('https://www.youtube.com/shorts/YJpznYgZlKM')), str)
+
